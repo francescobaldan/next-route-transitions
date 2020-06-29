49db065b9eda1b295a2af67f7726b508d7b1573e
francesco_baldan <francesco@mabiloft.com>
2020-06-29 21:57:46 +0200

    First commit

---
+++ pages/example.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+
+const Example = () => {
+  return (
+    <div>
+      <h2>This is an example</h2>
+    </div>
+  );
+};
+
+export default Example;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+    </div>
+  );
+};
+
+export default Home;
